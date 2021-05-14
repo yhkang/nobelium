20151b5be1d8959dd5fad25e11677e60b644d2e5
Kang <yahuakang@gmail.com>
2021-05-14 23:59:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://twitter.com/yhkangCS',
+  socialLink: 'https://twitter.com/yhkang001',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
