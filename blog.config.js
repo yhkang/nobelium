a55dbdc184bd85e12bf562949027ce2b50b89692
yahuakang <yahuakang@gmail.com>
2021-10-11 16:17:53 +0800

    修改配置

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og-image.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://t.me/yhkang',
   seo: {
     keywords: ['Blog', 'Website', 'YHKANG'],
